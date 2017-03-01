be1d55939e802949764a302de4ae3593b8e5b4c9
isumizumi <isumi.karina72@gmail.com>
2017-03-01 14:26:26 +0700

    Release 2 API: TWitter

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,37 @@
 var express = require('express');
 var router = express.Router();
-var OAuth = require('oauth');
 require('dotenv').config()
+
+var OAuth = require('oauth');
+router.post('/tweet', function(req, res, next) {
+  var oauth = new OAuth.OAuth(
+     'https://api.twitter.com/oauth/request_token',
+     'https://api.twitter.com/oauth/access_token',
+     process.env.Consumer_Key,
+     process.env.Application_Secret,
+     '1.0A',
+     null,
+     'HMAC-SHA1'
+   );
+   var key = req.body.search
+   oauth.get(
+     'https://api.twitter.com/1.1/search/tweets.json?q='+key,
+     process.env.User_Token, //test user token
+     process.env.User_Secret, //test user secret
+    //  function (e, data, res){ --> res change to cb (biar gak bentrok sama res di function pertama)
+     function (e, data, cb){
+       if (e) console.error(e);
+       console.log(require('util').inspect(data));
+      //  done();
+      res.send(data)
+     });
+});
+
 var meetup = require('meetup-api');
 var meetupObject = meetup({key: process.env.API_Key})
-
-router.get('/', function(req, res, next) {
+router.post('/meetup', function(req, res, next) {
   meetupObject.getGroup({
-      urlname: 'Drupal-Jakarta'
+      urlname: req.body.url
   }, function(err, resp) {
       if (err) {
           res.send('Found meetup error', err);
@@ -16,23 +40,20 @@ router.get('/', function(req, res, next) {
   });
 });
 
-// router.get('/', function(req, res, next) {
-//   var OAuth2 = OAuth.OAuth2;
-//      var twitterConsumerKey = process.env.Client_ID;
-//      var twitterConsumerSecret = process.env.Client_Secret;
-//      var oauth2 = new OAuth2(process.env.API_Key,
-//        twitterConsumerSecret,
-//        'https://api.meetup.com/',
-//        null,
-//        '2/categories',
-//        null);
-//      oauth2.getOAuthAccessToken(
-//        '',
-//        {'grant_type':'client_credentials'},
-//        function (e, access_token, refresh_token, results){
-//        console.log('bearer: ',e);
-//        res.send(results)
-//      });
+// var graph = require('fbgraph');
+// var authUrl = graph.getOauthUrl({key: process.env.App_ID_FB})
+// console.log(graph);
+// console.log(authUrl);
+
+// var searchOptions = {
+//   q:     "drupal",
+//   type:  "post"
+// };
+//
+// router.get('/page', function(req, res, next) {
+//   authUrl.search(searchOptions, function(err, res) {
+//     res.send(res); // {data: [{id: xxx, from: ...}, {id: xxx, from: ...}]}
+//   });
 // });
 
 module.exports = router;
