275722831a48f8ff16fd5bdfeea4f24e712777e8
adamsaparudin <adamsaparudin@gmail.com>
2017-03-01 13:08:38 +0700

    Part

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var LYQL = require('lyql');
+var TelegramBot = require('telegrambot');
+var api = new TelegramBot('310129702:AAHXig3BC7_agFCXoPJdhCf-iQUrjUSr9YM');
+
+var options = {
+  "Stocks": ["BRMS.JK", "DOID.JK", "BWPT.JK", "ENRG.JK"],
+  // Price, Change, and Volume
+  "Parameters": ["l84", "p43", "v53"]
+}
+
+var check = {
+  "BRMS.JK" : "95.00"
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  var test = new LYQL(options, function(data){
+  console.log(data);
+  res.render('index', {test: data})
+
+});
+
+test.start();
 });
 
 module.exports = router;
--- test.js
+++ test.js
@@ -3,24 +3,33 @@ var TelegramBot = require('telegrambot');
 var api = new TelegramBot('310129702:AAHXig3BC7_agFCXoPJdhCf-iQUrjUSr9YM');
 
 var options = {
-  "Stocks": ["BRMS.JK"],
+  "Stocks": ["BRMS.JK", "DOID.JK", "BWPT.JK", "ENRG.JK"],
   // Price, Change, and Volume
   "Parameters": ["l84", "p43", "v53"]
 };
 
-var check = {
-  "BRMS.JK" : "95.00"
-}
+var check = [
+  {stocks : "BRMS.JK", price : "95.00"},
+  {stocks : "DOID.JK", price : "378.00"},
+  {stocks : "TLKM.JK", price : "3870.00"},
+  {stocks : "ENRG.JK", price : "50.00"}
+]
 
 var test = new LYQL(options, function(data){
-  test = data['BRMS.JK']
-  console.log(test.l84);
-  if(test.l84 == '94.00') {
-    api.sendMessage({ chat_id: 130776262, text: 'BRMS Breakout price' }, function (err, message) {
-        if (err) throw err;
-        console.log(message);
-    });
+  for(let i=0; i<check.length; i++) {
+    if(data[check[i].stocks]) {
+      if(data[check[i].stocks])
+    }
   }
+  // console.log(data);
+  // brms = data['BRMS.JK']
+  // console.log(test.l84);
+  // if(brms.l84 == '94.00') {
+  //   api.sendMessage({ chat_id: 130776262, text: 'BRMS Breakout price' }, function (err, message) {
+  //       if (err) throw err;
+  //       console.log(message);
+  //   });
+  // }
 });
 
 test.start();
